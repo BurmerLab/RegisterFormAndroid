f03b2f2bb04ab291982e16deceb048cf8a4dbccd
HetmanJIIIS <hetmanjiiis@gmail.com>
2015-12-28 22:01:13 +0100

    Added work and home localization to form registration.

--- app/src/main/java/com/example/michal/geolocalization/GeolocalizationResources.java
+++
@@ -1,9 +0,0 @@
-package com.example.michal.geolocalization;
-
-/**
- * Created by Michał on 2015-12-07.
- */
-public class GeolocalizationResources {
-
-    public static final float MAP_ZOOM_ALTITUDE = 13;
-}
