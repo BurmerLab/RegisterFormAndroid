83b92a8179e2a1c70e7aabe75611afdb4c8b9c8e
burmerlab <hetmanjiiis@gmail.com>
2016-02-07 20:07:51 +0100

    Changes for Home and Work place register activity

---
+++ app/src/main/java/com/mytway/properties/PropertiesValues.java
@@ -0,0 +1,12 @@
+package com.mytway.properties;
+
+public class PropertiesValues {
+
+    public final static int HOURS_WORK_LENGTH_TIME_REGISTRATION = 8;
+    public final static int MINUTS_WORK_LENGTH_TIME_REGISTRATION = 0;
+    public final static int HOURS_START_WORK_TIME_REGISTRATION = 8;
+    public final static int MINUTES_START_WORK_TIME_REGISTRATION = 0;
+    public static int PASSWORD_LENGTH_REQUIRED = 6;
+
+    public static int WORK_AND_HOME_PLACE_MAP_ZOOM_LEVEL = 16;
+}
